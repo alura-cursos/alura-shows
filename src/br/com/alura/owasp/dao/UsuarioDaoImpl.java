30ca2733bd8d2852023fe701dbe287b938236f9f
alexfelipe <alexfelipevieira@gmail.com>
2017-10-20 14:31:46 -0200

    Aula1-5

--- src/br/com/alura/owasp/dao/UsuarioDaoImpl.java
+++ src/br/com/alura/owasp/dao/UsuarioDaoImpl.java
@@ -1,60 +1,29 @@
 package br.com.alura.owasp.dao;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.owasp.model.Usuario;
-import br.com.alura.owasp.util.ConnectionFactory;
 
 @Repository
 public class UsuarioDaoImpl implements UsuarioDao {
 
-	Connection connection = new ConnectionFactory().getConnection();
+	@PersistenceContext
+	private EntityManager manager;
 
 	public void salva(Usuario usuario) {
-		String query = "insert into USUARIO (email,senha,nome,nomeImagem) values ('"
-				+ usuario.getEmail()
-				+ "','"
-				+ usuario.getSenha()
-				+ "','"
-				+ usuario.getNome() + "','" + usuario.getNomeImagem() + "');";
-		try {
-			Statement statement = connection.createStatement();
-			statement.executeUpdate(query);
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
+		manager.persist(usuario);
 	}
 
 	public Usuario procuraUsuario(Usuario usuario) {
-		String query = "SELECT * FROM USUARIO WHERE email=" + "'"
-				+ usuario.getEmail() + "'" + " and senha=" + "'"
-				+ usuario.getSenha() + "';";
-		try {
-			Statement statement = connection.createStatement();
-			ResultSet results = statement.executeQuery(query);
-			Usuario usuarioRetorno = new Usuario();
-			while (results.next()) {
-				usuarioRetorno.setEmail(results.getString("email"));
-				usuarioRetorno.setSenha(results.getString("senha"));
-				usuarioRetorno.setNomeImagem(results.getString("nomeImagem"));
-				usuarioRetorno.setNome(results.getString("nome"));
-				if(usuarioRetorno.getEmail().equals(usuario.getEmail())){
-					break;
-				}
-			}
-			if (usuarioRetorno.getEmail() == null
-					&& usuarioRetorno.getSenha() == null) {
-				return null;
-			} else {
-				return usuarioRetorno;
-			}
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
+		TypedQuery<Usuario> query = manager
+				.createQuery("select u from Usuario u where u.email=:email and u.senha=:senha ", Usuario.class);
+		query.setParameter("email", usuario.getEmail());
+		query.setParameter("senha", usuario.getSenha());
+		Usuario usuarioRetornado = query.getResultList().stream().findFirst().orElse(null);
+		return usuarioRetornado;
 	}
 }
